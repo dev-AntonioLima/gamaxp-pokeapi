6b94c632c80cfe9005a8aeb5a6323225bda5f08d
Antonio Lima <antonio55senac@gmail.com>
2020-09-06 16:56:43 +0000

    Consumindo a API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Dashboard from './pages/Dashboard';
 import './app.css';
 import Header from './components/Header';
+import axios from 'axios';
+
+
+
+
+
+ axios.get("https://pokeapi.co/api/v2/pokemon?limit=50").then(response => console.log(response));
+
+
 function App() {
   return (
     <>
     <Header/>
     <div className='App'>
-      <Dashboard></Dashboard>
+      <Dashboard>
+   
+      </Dashboard>
     </div>
     </>
   );
